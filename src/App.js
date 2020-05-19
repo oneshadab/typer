ce88d590fe4af7fdfb28784b002923497d234fb8
Shadman <oneshadab@gmail.com>
2020-05-20 01:15:45 +0600

    Add root app component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "bulma/css/bulma.css";
+
+import React from "react";
+
+import Typer from "./components/Typer";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Typer />
+    </div>
+  );
+}
